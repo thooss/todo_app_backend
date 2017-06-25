41edb9e3d47d9344c983f796d460a1832fd84f33
Thomas Hoos <thoos@cps.edu>
2017-06-25 14:51:09 +0000

    Added some more stuffs

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,27 +3,6 @@ class TodoController < ApplicationController
     end
     def show 
         todo_id=params[:id]
-        if todo_id=='1' 
-            @pomodoro_estimate=7
-            @todo_description="Take a Nap"
-        elsif todo_id=='2'
-            @pomodoro_estimate=6
-            @todo_description="sleep"
-        elsif todo_id=='3'
-            @pomodoro_estimate=5
-            @todo_description="Dream"
-        elsif todo_id=='4'
-            @pomodoro_estimate=4
-            @todo_description="Slumber"
-        elsif todo_id=='5'
-            @pomodoro_estimate=3
-            @todo_description="Hibernate"
-        elsif todo_id=='6'
-            @pomodoro_estimate=2
-            @todo_description="A suspension of consciousness"
-        elsif todo_id=='7'
-            @pomodoro_estimate=1
-            @todo_description="Rest"
-        end
+        @todo=Todo.find_by_id(todo_id)
     end
 end
