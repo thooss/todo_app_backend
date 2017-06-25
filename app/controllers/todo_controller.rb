8dda8f50b53e30a89dbfd2e7641f4314a4b2d76c
Thomas Hoos <thoos@cps.edu>
2017-06-25 15:52:02 +0000

    Edited in some edits

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -13,5 +13,17 @@ class TodoController < ApplicationController
         t.pomodoro_estimate=params["pomodoro-estimate"]
         t.save
         redirect_to "/todo/show/#{t.id}"
+    end 
+    def edit 
+        todo_id=params[:id]
+        @todo=Todo.find_by_id(todo_id)
+    end
+    def update 
+        todo_id=params[:id]
+        t=Todo.find_by_id(todo_id)
+        t.description=params["description"]
+        t.pomodoro_estimate=params["pomodoro-estimate"]
+        t.save
+        redirect_to "/todo/show/#{t.id}" 
     end
 end
